bfe4ca62e7a64c169fe4af149d74b164a5e3fe36
Rex-Chiang <m41045@gmail.com>
2020-04-21 10:50:36 +0800

    Fix Crawler2 bugs. Download user picture that earn most of likes.

--- Crawler2.py
+++ Crawler2.py
@@ -25,53 +25,43 @@ class Crawler1:
 
     def ProInfo(self, content):
         like = dict()
-        comment = dict()
-        # 文章圖片網址、愛心數、留言數的正則表達式   
-        LIKEre = re.compile(r"(https:\/\/[\w\W]*)(\",\"edge_liked_by\":{\"count\":)(\d*)")
-        COMre = re.compile(r"(edge_media_to_comment\":{\"count\":)(\d*)")
+        # 文章圖片網址、愛心數的正則表達式   
+        LIKEre = re.compile(r"(\"display_url\"\:\")(https:\/\/[\w\W]*)(\",\"gating_info[\w\W]*)(,\"edge_liked_by\":{\"count\":)(\d*)")
         content = content.split("shortcode") # 每篇文章是以屬性shortcode作分段，其中個人照片網址只存在於第一段
 
-        print(content)
         for article in content:
-            # 取得文章圖片網址、愛心數、留言數
+            # 取得文章圖片網址、愛心數
             getlike = LIKEre.search(article)
-            getcomment = COMre.search(article)
-     
+            
             if getlike != None:
-                # 將文章圖片網址、愛心數、留言數紀錄於字典
-
-                like[getlike.group(1)] = int(getlike.group(3).replace(",",""))
-                comment[ getlike.group(1)] = int(getcomment.group(2).replace(",",""))
-                
-        print(like)
-        print(comment)
-      
-        return like, comment
-
-    def Statistic(self, like, comment):
-        # 原本的like、comment字典形式為[圖片網址:愛心數]、[圖片網址:留言數]
-        # 為了以愛心數、留言數做鍵查詢將字典形式改為[愛心數:圖片網址]、[留言數:圖片網址]
-        TransLike = {v : k for k, v in like.items()}
-        TransComm = {v : k for k, v in comment.items()}
-        # 取得最高愛心數、最高留言數、最低愛心數、最低留言數文章
-        Most_Liked_Posts = TransLike[max(like.values())]
-        Most_Commented_Posts = TransComm[max(comment.values())]
+                # 將文章圖片網址、愛心數紀錄於字典
+                pic_url = getlike.group(2).replace("\\u0026", "&")
+                like_ct = getlike.group(5)
+                like[pic_url] = like_ct
         
-        return Most_Liked_Posts, Most_Commented_Posts
+        return like
+
+    def Statistic(self, like):
+        # 原本的like字典形式為[圖片網址:愛心數]
+        # 為了以愛心數做鍵查詢將字典形式改為[愛心數:圖片網址]
+        TransLike = {v : k for k, v in like.items()}
+        # 取得最高愛心數
+        Most_Liked_Posts = TransLike[max(like.values())]
+        
+        return Most_Liked_Posts
     
-    def SaveImage(self, img, path, account):
-        img_path = os.path.join("C:/Users/m4104/Desktop/InstagramBot-project/"+path+"/"+account)
-        urlretrieve(img, img_path)
+    def SaveImage(self, img, account):
+        img_path = os.path.join("C:/Users/m4104/Desktop/InstagramBot-project/mostlike/"+account)        
+        urlretrieve(img, img_path + ".png")
         
     def Run(self, account="ID"):
-        like, comment = self.ProInfo(self.script)
-        Most_Liked_Posts, Most_Commented_Posts = self.Statistic(like, comment)
+        like = self.ProInfo(self.script)
+        Most_Liked_Posts = self.Statistic(like)
         
-        # 將個人照片、最最高愛心數、最高留言數、最低愛心數、最低留言數文章圖片儲存
-        self.SaveImage(Most_Liked_Posts, "mostlike", account)
-        self.SaveImage(Most_Commented_Posts, "mostcomment", account)
+        # 將個人照片、最最高愛心數文章圖片儲存
+        self.SaveImage(Most_Liked_Posts, account)
         
-        return like[Most_Liked_Posts], comment[Most_Commented_Posts]
+        return like[Most_Liked_Posts]
 
 if __name__ == '__main__':
     
@@ -80,5 +70,4 @@ if __name__ == '__main__':
     
     Crawler = Crawler1(url)
     followers, followed, article = Crawler.RE(Crawler.script)
-    Most_Liked_Posts, Most_Commented_Posts  = Crawler.Run()
-    #Most_Liked_Posts, Most_Commented_Posts, Least_Liked_Posts, Least_Commented_Posts = Crawler.Statistic(like, comment)
+    Most_Liked_Posts  = Crawler.Run(ID)
